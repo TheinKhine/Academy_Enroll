0b508b7453e1edef55177df96508befe8041d9fa
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-05-21 08:23:42 +0630

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard/index');
+})->middleware('auth');
 
 Auth::routes();
 
